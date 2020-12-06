aaa99ff376a9d5b7b014ebcf22c87b9d34dbbaaa
Anwesh43 <anweshthecool0@gmail.com>
2020-12-06 20:06:17 +0530

    adding SquareExtendLineFill class

--- index.ts
+++ index.ts
@@ -189,3 +189,26 @@ class SELFNode {
         return this
     }
 }
+
+class SquareExtendLineFill {
+
+    curr : SELFNode = new SELFNode(0)
+    dir : number = 1 
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
